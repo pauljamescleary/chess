94603aa0555f634cd3c47c155dcdb5a696390dbf
Paul James Cleary <pauljamescleary@gmail.com>
2022-12-12 15:58:06 -0500

    Docker working again

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -6,11 +6,13 @@ echo "SQLALCHEMY_DATABASE_URI = $SQLALCHEMY_DATABASE_URI"
 echo "Files..."
 ls -al
 
+export FLASK_APP=chess/app
+
 echo "Running migrations..."
 flask db upgrade
 
 echo "Starting app..."
-flask run --app chess/app --host=0.0.0.0
+flask run --host=0.0.0.0
 
 # Not sure why we want to run gunicorn here???
 # gunicorn -c gunicorn.config.py wsgi:app
