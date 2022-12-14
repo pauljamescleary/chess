828f4ef5a7ebedf10793d6459ba155fe0a0ff077
Paul James Cleary <pauljamescleary@gmail.com>
2022-12-14 14:14:06 -0500

    Got tests to work again

--- backend/chess/app.py
+++ backend/chess/app.py
@@ -1,5 +1,7 @@
 from . import create_app
 
+app = create_app('dev')
+
 if __name__ == '__main__':
     print("*** IN APP.PY ***")
     create_app().run(debug=True, host='0.0.0.0')
--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -1,5 +1,6 @@
 import pytest
 import uuid
+import base64
 
 # client comes from pytest-flask
 
@@ -11,35 +12,6 @@ def test_create_user(client, sample_user):
     assert resp.status_code == 200
 
 
-def test_login(client, sample_user):
-    user = {"email": sample_user.email, "password": "password"}
-    resp = client.post('/login', json=user)
-    assert resp.status_code == 200
-
-    # Find the session cookie, should be present
-    cookie = next(
-        (cookie for cookie in client.cookie_jar if cookie.name == "session"), None)
-    assert cookie is not None
-
-
-def test_logout(client, sample_user):
-    user = {"email": sample_user.email, "password": "password"}
-    resp = client.post('/login', json=user)
-    assert resp.status_code == 200
-
-    # Find the session cookie, should be present
-    cookie = next(
-        (cookie for cookie in client.cookie_jar if cookie.name == "session"), None)
-    assert cookie is not None
-
-    # Logout
-    resp = client.delete('/login')
-    assert resp.status_code == 200
-    cookie = next(
-        (cookie for cookie in client.cookie_jar if cookie.name == "session"), None)
-    assert cookie is None
-
-
 def test_get_users(client, sample_user):
     resp = client.get('/users')
     assert resp.status_code == 200
@@ -47,3 +19,12 @@ def test_get_users(client, sample_user):
     # we should have our sample user in there
     user_emails = list(map(lambda x: x['email'], users))
     assert sample_user.email in user_emails
+
+def test_high_score(client, sample_user):
+    score = { "score": 100, "level": 1}
+    auth_string = f"{sample_user.email}:password"
+    auth_ascii = auth_string.encode('ascii')
+    auth_b64 = base64.b64encode(auth_ascii)
+    auth = { 'Authorization': "Basic " + auth_b64.decode('ascii') }
+    resp = client.post('/scores', headers=auth, json=score)
+    assert resp.status_code == 200
