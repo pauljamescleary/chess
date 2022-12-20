160b70f1e41f8bf409f4f28fbf7d23700ed21891
Paul James Cleary <pauljamescleary@gmail.com>
2022-12-20 11:52:46 -0500

    Getting build to work anywhere

--- frontend/build.sh
+++ frontend/build.sh
@@ -2,6 +2,7 @@
 
 # This assumes poetry, gets where poetry stores its dependencies
 # VIRTUAL_ENV_PATH=`poetry show -v 2> /dev/null | head -n1 | cut -d ' ' -f 3`
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Clean up anything just in case
 rm -rf dist
@@ -12,15 +13,14 @@ pyinstaller \
 --windowed \
 --onefile \
 -n chessgame \
---clean \
---add-data "chess/assets/bishop.png:assets" \
---add-data "chess/assets/boo.mp3:assets" \
---add-data "chess/assets/clack.mp3:assets" \
---add-data "chess/assets/correct.wav:assets" \
---add-data "chess/assets/gameDefinitions.json:assets" \
---add-data "chess/assets/Hide.png:assets" \
---add-data "chess/assets/LoginButtonImage.png:assets" \
---add-data "chess/assets/queen.png:assets" \
---add-data "chess/assets/rook.png:assets" \
---add-data "chess/assets/Show.png:assets" \
-entry_point.py
+--add-data "${SCRIPT_DIR}/chess/assets/bishop.png:assets" \
+--add-data "${SCRIPT_DIR}/chess/assets/boo.mp3:assets" \
+--add-data "${SCRIPT_DIR}/chess/assets/clack.mp3:assets" \
+--add-data "${SCRIPT_DIR}/chess/assets/correct.wav:assets" \
+--add-data "${SCRIPT_DIR}/chess/assets/gameDefinitions.json:assets" \
+--add-data "${SCRIPT_DIR}/chess/assets/Hide.png:assets" \
+--add-data "${SCRIPT_DIR}/chess/assets/LoginButtonImage.png:assets" \
+--add-data "${SCRIPT_DIR}/chess/assets/queen.png:assets" \
+--add-data "${SCRIPT_DIR}/chess/assets/rook.png:assets" \
+--add-data "${SCRIPT_DIR}/chess/assets/Show.png:assets" \
+${SCRIPT_DIR}/entry_point.py
